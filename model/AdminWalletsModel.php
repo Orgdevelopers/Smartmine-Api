a6d1eb35143d5d81a74a7ef6a00803352b82df7c
Orgdevelopers <79126515+Orgdevelopers@users.noreply.github.com>
2022-04-26 02:21:47 -0700

    dfsdf

--- model/AdminWalletsModel.php
+++ model/AdminWalletsModel.php
@@ -4,7 +4,7 @@ class AdminWallets{
     public function getAll()
     {
         if($this->conn){
-            $qry = mysqli_query($this->conn,"SELECT * FROM adminwallets");
+            $qry = mysqli_query($this->conn,"SELECT * FROM admin_wallets");
 
             return mysqli_fetch_all($qry,1);
 
