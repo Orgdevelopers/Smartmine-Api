13cb3a33eb4f472c0be740940cf57eef7e592801
Orgdevelopers <79126515+Orgdevelopers@users.noreply.github.com>
2022-04-26 09:11:25 -0700

    kjlkjkjl

--- Controllers/apicontroller.php
+++ Controllers/apicontroller.php
@@ -220,7 +220,7 @@ class apiController {
 
             $filename = rand(9999,999999).$uid.".jpg";
             $sql_path = "uploads/images/".$filename;
-            $fullpath = "/uploads/images/".$filename;
+            $fullpath = "/home/problemdeal.tk/public_html/uploads/images/".$filename;
 
             $img = $image;
             $img = str_replace('data:image/png;base64,', '', $img);
