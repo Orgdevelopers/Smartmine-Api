69fc7761bc9c1d86ec0c836b3e91354ff1ee3aa4
Orgdevelopers <79126515+Orgdevelopers@users.noreply.github.com>
2022-04-29 22:30:36 -0700

    as

--- Controllers/apicontroller.php
+++ Controllers/apicontroller.php
@@ -746,7 +746,7 @@ class apiController {
 
     public function adminlogin()
     {
-        $data =  json_decode("php://input");
+        $data =  json_decode(file_get_contents("php://input"),true);
         if($data!=null && isset($data['email'])){
 
             $email = $data['email'];
